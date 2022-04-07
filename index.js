2b945267c160e393fab01cdaa1cd2acd44c75e36
Devin Ho <djxho36@gmail.com>
2022-04-07 17:06:42 -0400

    commit tree 6afe40c833eacc68aa07abeaaf525d8c6b5574a1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork (choose = "go to the office"){
+    return (`This Monday, I will ${choose}.`);
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(special = "*"){
+    return function (type = "a hard worker"){
+        return `You are ${special}${type}${special}!`;
+    }
+}
+wrapAdjective();
+wrapAdjective("||")("a dedicated programmer");
